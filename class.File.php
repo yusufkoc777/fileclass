1fceedf49b2359c254ed021aae1ed40a178d8050
yusuf6554 <yusuf_koc_777@windowslive.com>
2014-05-17 14:26:55 +0300

    Version 1.1
    Version 1.1 release

--- class.File.php
+++ class.File.php
@@ -1,6 +1,7 @@
 <?php
 class File {
 	public $contents;
+	public $bomMode = false;
 	public function __construct($contents="")
 	{
 		$this->contents = $contents;
@@ -13,14 +14,22 @@ class File {
 	}
 	public function saveFile($filename)
 	{
+		$c = $this->contents;
+		if ($this->bomMode) {
+			$c = "\xef\xbb\xbf".$this->contents;
+		}
 		$f = fopen($filename,'w');
-		fwrite($f,$this->contents);
+		fwrite($f,$c);
 		fclose($f);
 	}
 	public function loadFile($filename)
 	{
 		if(file_exists($filename)){
 			$this->contents = file_get_contents($filename);
+			if (strncmp($str, "\xef\xbb\xbf", 3)) == 0){
+				$this->bomMode = true;
+				$this->contents = substr($str, 3);
+			}
 		}else{
 			throw new FileNotFoundException("No such file or directory: ".$filename);
 		}
@@ -32,6 +41,10 @@ class File {
 	public function getContents()
 	{
 		return $this->contents;
+	}
+	public function setBOM($bomMode)
+	{
+		$this->bomMode = $bomMode;
 	}
 }
 class FileNotFoundException extends Exception {
